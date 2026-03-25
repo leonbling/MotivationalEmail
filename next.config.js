5fbed30b402300c1131df9a4f2506f2137149d10
leonbling <Leon-gray@hotmail.com>
2026-03-25 12:12:56 +0000

    Create next.config.js for Next.js configuration

---
+++ next.config.js
@@ -0,0 +1,10 @@
+'use strict';
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  // Add your custom Next.js configuration here
+};
+
+module.exports = nextConfig;
